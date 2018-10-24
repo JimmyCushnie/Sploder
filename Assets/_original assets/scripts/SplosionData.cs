ab604e040a9112acc32bd0f0b13c1ea84a7e2c3f
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-10-24 02:11:45 -0400

    added SplosionData classes

---
+++ Assets/_original assets/scripts/SplosionData.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+namespace Splosions
+{
+    public class SplosionData
+    {
+        public float3 OriginPosition;
+        public float3 OriginEulerAngles;
+
+        public float3 SpeedsMin;
+        public float3 SpeedsMax;
+
+        public bool Rounded;
+
+        public List<Color> ColorProbabilities = new List<Color>();
+        public List<Mesh> MeshProbabilities = new List<Mesh>();
+    }
+
+    public class SplosionDataWithScale : SplosionData
+    {
+        public float3 ScalesMin;
+        public float3 ScalesMax;
+
+        public float3 ScaleSpeedsMin;
+        public float3 ScaleSpeedsMax;
+    }
+
+    public class SplosionDataWithRotation : SplosionData
+    {
+        public float3 RotationsMin;
+        public float3 RotationsMax;
+
+        public float3 RotationSpeedsMin;
+        public float3 RotationSpeedsMax;
+    }
+
+    public class SplosionDataWithRotationAndScale : SplosionDataWithRotation // god damn it c# why u no multiple inheritance
+    {
+        public float3 ScalesMin;
+        public float3 ScalesMax;
+
+        public float3 ScaleSpeedsMin;
+        public float3 ScaleSpeedsMax;
+    }
+}
